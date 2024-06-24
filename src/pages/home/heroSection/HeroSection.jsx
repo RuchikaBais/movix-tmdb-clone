57cfd5338a53b03fd37308eb2d20740de4f59864
RuchikaBais <ruchikasbais@gmail.com>
2024-06-24 21:19:12 +0530

    added alert

--- src/pages/home/heroSection/HeroSection.jsx
+++ src/pages/home/heroSection/HeroSection.jsx
@@ -17,15 +17,34 @@ function HeroSection() {
   const { url } = useSelector((state) => state.home)
   const { data, loading } = useFetch("/movie/upcoming")
 
+
   useEffect(() => {
-        const bg = url?.backdrop ? (url?.backdrop + data?.results[Math.floor(Math.random() * 20)]?.backdrop_path) : bg2  ;
+    const alertShown = localStorage.getItem('alertShown');
 
+    if (!alertShown) {
+      alert(`This application uses The Movie Database (TMDb) API for data fetching. Please note that the content may be inaccessible on Jio networks due to restrictions; using a different network or VPN is recommended.`);
+      localStorage.setItem('alertShown', 'true');
+    }
+    
+    const handleBeforeUnload = () => {
+      localStorage.removeItem('alertShown');
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  useEffect(() => {
+    const bg = url?.backdrop ? (url?.backdrop + data?.results[Math.floor(Math.random() * 20)]?.backdrop_path) : bg2;
     setbackground(bg);
-
   }, [data])
 
   const searchQueryHandler = (e) => {
-    if (e.key === "Enter" && query.length > 0) {
+    console.log(e)
+
+    if ((e.key === 'Enter' || e.target.tagName === 'BUTTON' ) && query.length > 0) {
       navigate(`/search/${query}`)
     }
   }
@@ -35,27 +54,27 @@ function HeroSection() {
 
       {!loading &&
         <div className="backdrop-img">
-          <Img src={background}/>
+          <Img src={background} />
         </div>}
 
-        <div className="opacity-layer">
+      <div className="opacity-layer">
 
-        </div>
+      </div>
 
       <ContentWrapper>
-          <div className="heroSectionContent">
-            <span className="title">Welcome</span>
-            <span className="sub-title">Millions of movies, TV shows and people to discover.
-              Explore now.</span>
-            <div className="searchInput">
-              <input
-                type="text"
-                placeholder='Search for movie or TV show...'
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyUp={searchQueryHandler} />
-              <button>Search</button>
-            </div>
+        <div className="heroSectionContent">
+          <span className="title">Welcome</span>
+          <span className="sub-title">Millions of movies, TV shows and people to discover.
+            Explore now.</span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder='Search for movie or TV show...'
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler} />
+            <button onClick={searchQueryHandler}>Search</button>
           </div>
+        </div>
       </ContentWrapper>
 
     </div>
